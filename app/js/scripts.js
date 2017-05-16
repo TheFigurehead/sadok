47e7e52f3dae0d8ef9808a1d303a0e95090cc8ea
Anna <lastkindnesss@gmail.com>
2017-05-16 18:07:54 +0300

    Added header + carousel sadok

--- app/js/scripts.js
+++ app/js/scripts.js
@@ -10,3 +10,21 @@ function initMap() {
     map: map
   });
 }
+
+// ---------------
+// ***carousel****
+// ---------------
+
+$(document).ready(function(){
+  $('.top-slider').slick({
+  dots: true,
+  infinite: true,
+  speed: 500,
+  fade: true,
+  cssEase: 'linear'
+  });
+});
+
+// ---------------
+// *End carousel**
+// ---------------
