707b8e4595a660bc20335cdc571b65d887ccd051
Alexey <alex96813@gmail.com>
2017-05-19 11:46:53 +0300

    Lol wat?

--- app/js/scripts.js
+++ app/js/scripts.js
@@ -10,3 +10,36 @@ function initMap() {
     map: map
   });
 }
+
+//Portfolio
+var Portfolio = function() {};
+Portfolio.prototype.setSlider = function(slider, auto, num) {
+	Slider.prototype.setSlider.call(this, slider, auto, num);
+	this.changeActive = function(prev, next) {
+		this.items[prev].classList.remove("active");
+		this.items[next].classList.add("active");
+		this.nav[prev].classList.remove('active-nav');
+		this.nav[next].classList.add('active-nav');
+	}
+	this.nav = slider.getElementsByClassName('img-nav');
+	var _this = this;
+	forEach(this.nav, function(el, i) {
+		el.setAttribute('data-id', i);
+		el.addEventListener('click', function() {
+			_this.changeActive(slider.getElementsByClassName("active-nav")[0].getAttribute('data-id'), el.getAttribute('data-id'));
+		})
+	});
+}
+
+//Lazy
+var Lazy = function(){};
+Lazy.scroll = function() {
+	var className = 'lazyLoading'
+	var lazy = document.getElementsByClassName(className);
+	forEach(lazy, function(el, i) {
+		if (el && (el.getBoundingClientRect().top - (window.innerHeight * 2)) < 0) {
+			el.classList.remove(className);
+			el.src = el.getAttribute('data-src');
+		}
+	});
+}
